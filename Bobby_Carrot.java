28669dac887b463e9ddd08863d0f9c2073b7bf7e
Valeria <kzn.2003valeria@gmail.com>
2022-10-07 23:35:01 +0300

    actions

--- Bobby_Carrot.java
+++ Bobby_Carrot.java
@@ -10,6 +10,9 @@ public class Bobby_Carrot {
         final int HEIGHT = 10;
         final int WIDTH = 10;
         final int nCARROT = 10;
+        char rabbit = '~';
+        char carrot = 'C';
+        char x = 'X';
 
         char[][] field = new char[WIDTH][HEIGHT];
         char[][] opened = new char[WIDTH][HEIGHT];
@@ -20,7 +23,10 @@ public class Bobby_Carrot {
                 field[i][j] = '_';
             }
         }
-        field[0][0] = '~';
+        int posW = 0;
+        int posH = 0;
+        field[posW][posH] = rabbit;
+
 
         // generate carrots
         Random r = new Random();
@@ -28,8 +34,8 @@ public class Bobby_Carrot {
         while (k < nCARROT) {
             int w = r.nextInt(WIDTH);
             int h = r.nextInt(HEIGHT);
-            if (field[w][h] != 'C') {
-                field[w][h] = 'C';
+            if (field[w][h] != carrot) {
+                field[w][h] = carrot;
                 k++;
             }
         }
@@ -39,20 +45,105 @@ public class Bobby_Carrot {
 
         boolean gameIsGoing = true;
         int score = 0;
+        int pos = 0;
         Scanner sc = new Scanner(System.in);
         while (gameIsGoing) {
-            System.out.println("Score: " + score);
-//            char action = sc.next();
 
-            /*
-             * считываем ход
-             * проверяем на морковку и на возможность перехода в клетку
-             *
-             * нельзя: повтор
-             * морковка: +1, заменяем на "Х" и передвигаемся на клетку
-             * пустой: заменяем на "Х" и передвигаемся
-             *
-             * */
+            String action = sc.next();
+            switch (action) {
+                case "d":
+                    pos = posH + 1;
+                    if (pos != HEIGHT) {
+                        while (field[posW][pos] == x) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    } else {
+                        while (pos == HEIGHT) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    }
+
+                    if (field[posW][pos] == carrot) {
+                        score++;
+                    }
+                    field[posW][posH] = x;
+                    posH = pos;
+                    field[posW][posH] = rabbit;
+                    break;
+
+                case "s":
+                    pos = posW + 1;
+                    if (pos != WIDTH) {
+                        while (field[pos][posH] == x) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    } else {
+                        while (pos == WIDTH) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    }
+
+                    if (field[pos][posH] == carrot) {
+                        score++;
+                    }
+                    field[posW][posH] = x;
+                    posW = pos;
+                    field[posW][posH] = rabbit;
+                    break;
+
+                case "w":
+                    pos = posW - 1;
+                    if (pos != -1) {
+                        while (field[pos][posH] == x) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    } else {
+                        while (pos == -1) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    }
+
+                    if (field[pos][posH] == carrot) {
+                        score++;
+                    }
+                    field[posW][posH] = x;
+                    posW = pos;
+                    field[posW][posH] = rabbit;
+                    break;
+
+                case "a":
+                    pos = posH - 1;
+                    if (pos != -1) {
+                        while (field[posW][pos] == x) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    } else {
+                        while (pos == -1) {
+                            System.out.println("Измените ход");
+                            action = sc.next();
+                        }
+                    }
+
+                    if (field[posW][pos] == carrot) {
+                        score++;
+                    }
+                    field[posW][posH] = x;
+                    posH = pos;
+                    field[posW][posH] = rabbit;
+                    break;
+
+            }
+
+            System.out.println("Score: " + score);
+            printField(WIDTH, HEIGHT, field);
+
         }
 
 
@@ -70,6 +161,7 @@ public class Bobby_Carrot {
     public static void rules() {
         System.out.println("\nWelcome to Bobby Carrot!\nRules:\n Press W for up\n Press D for right\n Press S for down\n Press A for left\n");
     }
+
 
 }
 
